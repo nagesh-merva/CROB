a80f0399a003c9aceccc1387e27f7f62b5a63390
nagesh-merva <officialnagesh.merva@gmail.com>
2024-02-17 22:39:28 +0530

    creating slider

---
+++ src/js/main.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let items = document.querySelectorAll('.slider .item');
+    let next = document.getElementById('next');
+    let prev = document.getElementById('prev');
+
+    let active = 0;
+    let intervalId;
+
+    function loadShow() {
+        let stt = 0;
+        items[active].style.transform = `none`;
+        items[active].style.zIndex = 1;
+        items[active].style.filter = 'none';
+        items[active].style.opacity = 1;
+        for (var i = active + 1; i < items.length; i++) {
+            stt++;
+            items[i].style.transform = `translateX(${120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(-1deg)`;
+            items[i].style.zIndex = -stt;
+            items[i].style.filter = 'blur(5px)';
+            items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        }
+        stt = 0;
+        for (var i = active - 1; i >= 0; i--) {
+            stt++;
+            items[i].style.transform = `translateX(${-120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(1deg)`;
+            items[i].style.zIndex = -stt;
+            items[i].style.filter = 'blur(5px)';
+            items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        }
+    }
+
+    function startSlider() {
+        intervalId = setInterval(function () {
+            loadShow();
+            active = (active + 1) % items.length;
+        }, 5000);
+    }
+
+    function stopSlider() {
+        clearInterval(intervalId);
+    }
+
+    loadShow();
+    startSlider();
+
+    next.onclick = function () {
+        active = active + 1 < items.length ? active + 1 : 0;
+        loadShow();
+        stopSlider();
+    };
+
+    prev.onclick = function () {
+        active = active - 1 >= 0 ? active - 1 : items.length - 1;
+        loadShow();
+        stopSlider();
+    };
+
+});
