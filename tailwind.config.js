0aebc4b2335766d99f7f83aff42d4b637d528906
nagesh-merva <officialnagesh.merva@gmail.com>
2024-02-25 21:02:19 +0530

    Updating index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ const plugin = require('tailwindcss/plugin');
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pacifico: ['Pacifico', 'cursive'],
+      },
+    },
   },
   plugins: [
     plugin(({ addComponents }) => {
