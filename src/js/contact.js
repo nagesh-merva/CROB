e395c9ce5bc92c3c10fbcd721838a0c0c8add2c6
nagesh-merva <officialnagesh.merva@gmail.com>
2024-03-12 00:39:16 +0530

    setting up contact form submission

---
+++ src/js/contact.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector('form')
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const formData = new FormData(form)
+
+        sendEmail(formData);
+        alert('Form submitted successfully!')
+    });
+
+    function sendEmail(formData) {
+        fetch('https://cro-b-backend.vercel.app/contact', {
+            method: 'POST',
+            body: formData,
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Email sent successfully:', data)
+            })
+            .catch(error => {
+                console.error('Error sending email:', error)
+            })
+    }
+})
